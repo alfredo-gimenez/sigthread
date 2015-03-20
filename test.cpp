2e233045e8c75f331e1cf193a05caab3d69a0b44
Alfredo Gimenez <gimenez1@llnl.gov>
2015-03-20 14:42:21 -0700

    use proc/pid/task so we can track threads made by any API

--- test.cpp
+++ test.cpp
@@ -1,60 +1,34 @@
-#include  <stdio.h>
-#include  <stdlib.h>
-#include  <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include  <sys/types.h>
-#include  <sys/syscall.h>
-#include  <signal.h>
-#include  <unistd.h>
+#include <unistd.h>
+#include <signal.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
 
 #include <pthread.h>
 #include <omp.h>
 
-#include <algorithm>
 #include <vector>
 
-std::vector<int> tidlist;
+DIR *proc_pid_task;
+struct dirent *ent;
 
-void ompt_init()
+std::vector<int> get_thread_list()
 {
-    // Add current thread to threadlist
-    int mpid = omp_get_thread_num();
-    int mtid = syscall(SYS_gettid);
-    tidlist[mpid] = mtid;
-}
+    std::vector<int> thread_list;
 
-void ompt_destroy()
-{
-    // Remove current thread from threadlist
-    int mpid = omp_get_thread_num();
-    tidlist[mpid] = -1;
-}
-
-void sig_handle_broadcast(int sig)
-{
-    int mtid = syscall(SYS_gettid);
-
-    printf("Thread %d received BROADCAST signal\n", mtid);
-
-    // Master thread broadcasts to all other threads
-    for(int i=0; i<tidlist.size(); i++)
+    rewinddir(proc_pid_task);
+    while((ent = readdir(proc_pid_task)) != NULL)
     {
-        int tid = tidlist.at(i);
-
-        // No thread here
-        if(tid == -1)
-            continue;
-
-        // Send signal to thread using tgkill
-        syscall(SYS_tgkill, getpid(), tid, SIGUSR2);
+        if(ent->d_name[0] != '.')
+        {
+            thread_list.push_back(atoi(ent->d_name));
+        }
     }
-}
-
-void sig_handle_thread(int sig)
-{
-    int mtid = syscall(SYS_gettid);
-
-    printf("Thread %d received THREAD signal\n", mtid);
+    return thread_list;
 }
 
 void* sampler(void *args)
@@ -68,13 +42,31 @@ void* sampler(void *args)
     }
 }
 
-
-int main()
+void sig_handle_thread(int sig)
 {
-    int n=4;
+    int mtid = syscall(SYS_gettid);
+    printf("Thread %d received THREAD signal\n", mtid);
+}
 
-    tidlist.resize(omp_get_max_threads(),-1);
+void sig_handle_broadcast(int sig)
+{
+    int mtid = syscall(SYS_gettid);
 
+    printf("Thread %d received BROADCAST signal\n", mtid);
+
+    // Master thread broadcasts to all other threads
+    std::vector<int> thread_list = get_thread_list();
+    for(int i=0; i<thread_list.size(); i++)
+    {
+        int tid = thread_list[i];
+
+        // Send signal to thread using tgkill
+        syscall(SYS_tgkill, getpid(), tid, SIGUSR2);
+    }
+}
+
+void init_signal_handlers()
+{
     // Register broadcast signal handler
     struct sigaction sact1;
     memset(&sact1, 0, sizeof(sact1));
@@ -86,17 +78,31 @@ int main()
     memset(&sact2, 0, sizeof(sact2));
     sact2.sa_handler = &sig_handle_thread;
     sigaction(SIGUSR2, &sact2, NULL);
+}
+
+int main()
+{
+    // Open task directory (for threads)
+    proc_pid_task = opendir("/proc/self/task");
+    if(proc_pid_task == NULL)
+    {
+        printf("Unable to open pid tasks!\n");
+        return 1;
+    }
+    get_thread_list();
+
+    // Initialize signal handlers
+    init_signal_handlers();
 
     // Create sampler thread
     pthread_t sampler_thread;
     pthread_create(&sampler_thread, NULL, &sampler, NULL);
 
+    int n=4;
     int i,j,x;
-#pragma omp parallel for private(i,j,x) shared(tidlist)
+#pragma omp parallel for private(i,j,x) 
     for(i=0; i<n; i++)
     {
-        ompt_init();
-
         // Do meaningless work
         x = rand();
         for(j=0; j<10; j++)
@@ -108,8 +114,6 @@ int main()
         // Validate meaninglessness
         // (so work doesn't get compiled out)
         printf("%d\n",x);
-
-        ompt_destroy();
     }
 
     return 0;
